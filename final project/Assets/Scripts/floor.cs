626bda251ab6aac08a205fa2915da0228705a1d0
s107081028 <stanleychang0914110366@gmail.com>
2024-09-11 14:00:43 +0800

    final

---
+++ final project/Assets/Scripts/floor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floor : MonoBehaviour
+{
+    private float moveSpeed = -20;
+    private float zBound = 150;
+    private float zOffset = -300;
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (transform.position.z >= zBound)
+        {
+            transform.position += new Vector3(0, 0, zOffset);
+        }
+    }
+}
